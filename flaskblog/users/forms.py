0fad6656725453df01e30d33fa581ce2f62a7fe3
aravinds-arv <aravinds.arv@pm.me>
2022-01-21 16:45:30 +0530

    blueprints implementation

--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, ValidationError
-from wtforms.validators import DataRequired, Length, Email, EqualTo
-from flaskblog.models import User
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_login import current_user
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
@@ -46,11 +46,6 @@ class UpdateAccountForm(FlaskForm):
             if user:
                 raise ValidationError('That email is already taken. Please use a different one.')
 
-class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
-    submit = SubmitField('Post')
-
 class RequestResetForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Request Password Reset')
@@ -63,3 +58,4 @@ class ResetPasswordForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Reset Password')
+    
